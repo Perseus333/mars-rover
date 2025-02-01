c70f350c3c3a9f91e80ce6a0839af1a37cc32dd9
Perseus333 <perseusmith73@gmail.com>
2025-02-01 13:06:17 +0000

    fix: update configuration

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "📃 M.E.M.E. Docs",
+    pageTitleSuffix: "The project documentation in one place",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "perseus333.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
