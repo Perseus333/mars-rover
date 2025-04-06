92795c57b5c3c202cf7ced336fa2c9b6d7545241
Perseus333 <perseusmith73@gmail.com>
2025-04-06 12:42:18 +0100

    feat(TESTING!): testing sensor

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -9,12 +9,16 @@
 
 void setup() {
     // mainSetup();
-    motorDrive(1, 255, 1000);
-    motorDrive(2, 255, 1000);
 }
 
 void loop() {
     // mainLoop();
-    motorDrive(1, 255, 1000);
-    motorDrive(2, 255, 1000);
+    if (detectDistance() < 200) {
+        motorBrake(1);
+        motorBrake(2);
+    }
+    else {
+        motorDrive(1, 255, 1000);
+        motorDrive(2, 255, 1000);
+    }
 }
